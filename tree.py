c338fb8ce30160c7d481414c51e9302691b3d87c
George Sparrow <sparrowg@iusb.edu>
2017-09-26 16:32:56 -0600

    Adds Chi Squared testing
    Adds function chi_squared, which takes an attribute, and compares its chi squared calculated value to the table value in chisquared.csv
    Returns true or false, as a signal or whether to reject the null hypothesis
    Removes whitespace from chisquared.csv

--- tree.py
+++ tree.py
@@ -12,14 +12,28 @@ class Tree(object):
         self.right = None
         self.data=[]
         self.headers=[]
+        self.chi_squared_headers=[]
+        self.chi_squared_data=[]
         self.comparator=[]
 
-    def file_read(self, filename):
+    def file_read(self, filename): #{{{
         with open(filename) as csvfile:
             reader = csv.DictReader(csvfile)
             self.headers=reader.fieldnames
             for row in reader:
                 self.data.append(row)
+    # }}}
+
+    def chi_squared_read(self, filename): #{{{
+        with open(filename) as csvfile:
+            reader = csv.DictReader(csvfile)
+            self.chi_squared_headers=reader.fieldnames
+            for row in reader:
+                self.chi_squared_data.append(row)
+        for row in self.chi_squared_data:
+            for column in row:
+                row[column]=float(row.get(column))
+    # }}}
 
     # Write File Output functions {{{
     def file_write_inorder(self, filename):
@@ -95,8 +109,9 @@ class Tree(object):
         self.write_inorder()
     # }}}
 
-    def add_data(self, datum):
+    def add_data(self, datum): #{{{
         self.data.append(datum)
+    # }}}
 
     def compute_max_information_gain(self, classifier): #{{{
         entropy_dictionary = {}
@@ -176,12 +191,56 @@ class Tree(object):
         return entropy_summation
         # }}}
 
-    def information_gain(self, classifier, attribute):
+    def information_gain(self, classifier, attribute): #{{{
         #print "base_entropy"
         #print self.base_entropy(classifier)
         #print "summation"
         #print self.entropy(classifier, attribute)
         return (self.base_entropy(classifier) - self.entropy(classifier, attribute))
+    # }}}
+
+    def chi_squared(self, attribute, probability): #{{{
+        attribute_values_count = {} #counts of the attribute values
+        attribute_values_set = set() #holds both the degrees of freedom, and helps calculate the expected value
+        degrees_of_freedom = 0
+        expected_value = 0.0
+        critical_value = 0.0 #from the chi_squared csv
+        temp_value = 0.0
+        chi_squared_value = 0.0
+        for datum in self.data:
+            attribute_values_set.add(datum.get(attribute))
+        attribute_values_count = dict.fromkeys(attribute_values_set, 0)
+        #print attribute_values_set
+        for datum in self.data:
+            attribute_values_count[datum.get(attribute)]+=1 
+        degrees_of_freedom=((len(attribute_values_set))-1)
+        #print degrees_of_freedom
+        expected_value=float(float(len(self.data))/float(len(attribute_values_set)))
+        #print expected_value
+        for key in attribute_values_count.keys():
+            temp_value = float(expected_value) - float(attribute_values_count.get(key))
+            #print "Expected value"
+            #print expected_value
+            #print "actual value"
+            #print float(attribute_values_count.get(key))
+            #print "Temp value"
+            #print temp_value
+            temp_value = float(temp_value * temp_value)
+            #print "Temp value"
+            #print temp_value
+            temp_value = float(temp_value/expected_value)
+            #print "Temp value"
+            #print temp_value
+            chi_squared_value += temp_value
+        degrees_fo_freedom=20
+        critical_value = self.chi_squared_data[(degrees_of_freedom-1)].get(probability)
+        print critical_value
+        print chi_squared_value
+        if (critical_value >= chi_squared_value):
+            return True #Null hypothesis is correct
+        else:
+            return False #Null hypothesis is rejected
+    #}}}
 
     def choose_comparator(self, classifier): #{{{
         same=True
@@ -217,12 +276,18 @@ class Tree(object):
     # }}}
 
 my_file='altitude.csv'
+chi_squared_file='chisquared.csv'
 #my_file='photos.csv'
 classifier='Class'
+PROBABILITY='0.050'
 root = Tree()
 root.file_read(my_file)
-root.choose_comparator(classifier)
-root.write()
+root.chi_squared_read(chi_squared_file)
+#print root.chi_squared_headers
+#print root.chi_squared_data
+#root.choose_comparator(classifier)
+#root.write()
+print root.chi_squared(classifier, PROBABILITY)
 #temp_classifier='Family'
 #print root.compute_max_information_gain(temp_classifier)
 #information_gain=root.information_gain(temp_classifier, 'Cartoon')
